65d1bf0cca1d47f5ecd333efd4349941dc74f38e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-12-14 19:57:30 -0800

    chore: tweak colors

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,15 @@ enum Commands {
     Wezterm,
 }
 
-pub const RED_HUE: f32 = 0.0;
+pub const RED_HUE: f32 = 6.0;
 pub const GREEN_HUE: f32 = 145.0;
-pub const YELLOW_HUE: f32 = 109.0;
+pub const YELLOW_HUE: f32 = 93.0;
 pub const BLUE_HUE: f32 = 264.0;
 pub const MAGENTA_HUE: f32 = 300.0;
 pub const CYAN_HUE: f32 = 210.0;
 
 pub const CHROMA_MAX: f32 = 0.37;
-pub const CHROMA_STD: f32 = 0.2;
+pub const CHROMA_STD: f32 = 0.24;
 
 type Color = Srgb<u8>;
 struct Colors {
@@ -51,13 +51,13 @@ fn generate_color_pair(l: f32, c: f32, hue: f32) -> (Color, Color) {
         let srgb = Srgb::<f32>::from_color(oklch.clamp());
         Srgb::<u8>::from_format(srgb)
     };
-    
+
     let bright = {
         let oklch = Oklch::new(l, c - 0.02, hue);
         let srgb = Srgb::<f32>::from_color(oklch.clamp());
         Srgb::<u8>::from_format(srgb)
     };
-    
+
     (normal, bright)
 }
 
@@ -68,7 +68,7 @@ fn generate_colors() -> Colors {
     let (red, red_bright) = generate_color_pair(0.6, CHROMA_STD + 0.1, RED_HUE);
     let (green, green_bright) = generate_color_pair(0.8, CHROMA_STD, GREEN_HUE);
     let (yellow, yellow_bright) = generate_color_pair(0.99, CHROMA_STD, YELLOW_HUE);
-    let (blue, blue_bright) = generate_color_pair(0.9, CHROMA_STD, BLUE_HUE);
+    let (blue, blue_bright) = generate_color_pair(0.8, CHROMA_STD, BLUE_HUE);
     let (magenta, magenta_bright) = generate_color_pair(0.7, CHROMA_STD, MAGENTA_HUE);
     let (cyan, cyan_bright) = generate_color_pair(0.8, CHROMA_STD, CYAN_HUE);
     let (white, white_bright) = generate_color_pair(1.45, 0.02, GREEN_HUE);
@@ -122,42 +122,67 @@ fn print_color(name: &str, normal: Color, bright: Color) {
 }
 
 fn generate_wezterm_config(colors: Colors) {
-    println!("[colors]");
-    println!("ansi = [");
-    println!("    \"#{:X}\",", colors.black);
-    println!("    \"#{:X}\",", colors.red);
-    println!("    \"#{:X}\",", colors.green);
-    println!("    \"#{:X}\",", colors.yellow);
-    println!("    \"#{:X}\",", colors.blue);
-    println!("    \"#{:X}\",", colors.magenta);
-    println!("    \"#{:X}\",", colors.cyan);
-    println!("    \"#{:X}\",", colors.white);
-    println!("]");
-    println!("background = \"#{:X}\"", colors.background);
-    println!("brights = [");
-    println!("    \"#{:X}\",", colors.black_bright);
-    println!("    \"#{:X}\",", colors.red_bright);
-    println!("    \"#{:X}\",", colors.green_bright);
-    println!("    \"#{:X}\",", colors.yellow_bright);
-    println!("    \"#{:X}\",", colors.blue_bright);
-    println!("    \"#{:X}\",", colors.magenta_bright);
-    println!("    \"#{:X}\",", colors.cyan_bright);
-    println!("    \"#{:X}\",", colors.white_bright);
-    println!("]");
-    println!("cursor_bg = \"#{:X}\"", colors.white);
-    println!("cursor_border = \"#{:X}\"", colors.white);
-    println!("cursor_fg = \"#{:X}\"", colors.black);
-    println!("foreground = \"#{:X}\"", colors.foreground);
-    println!("selection_bg = \"#{:X}\"", colors.white);
-    println!("selection_fg = \"#{:X}\"", colors.black);
-    println!();
-    println!("[metadata]");
-    println!("aliases = [\"dpc\"]");
-    println!("author = \"dpc (http://github.com/dpc)\"");
-    println!("name = \"dpc\"");
-    println!("origin_url = \"https://github.com/dpc/dpc-theme\"");
-    println!("wezterm_version = \"wezterm 0-unstable-2025-01-24\"");
-    println!();
+    println!(
+        r###"[colors]
+ansi = [
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+]
+background = "#{:X}"
+brights = [
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+    "#{:X}",
+]
+cursor_bg = "#{:X}"
+cursor_border = "#{:X}"
+cursor_fg = "#{:X}"
+foreground = "#{:X}"
+selection_bg = "#{:X}"
+selection_fg = "#{:X}"
+
+[metadata]
+aliases = ["dpc"]
+author = "dpc (http://github.com/dpc)"
+name = "dpc"
+origin_url = "https://github.com/dpc/dpc-theme"
+wezterm_version = "wezterm 0-unstable-2025-01-24"
+"###,
+        colors.black,
+        colors.red,
+        colors.green,
+        colors.yellow,
+        colors.blue,
+        colors.magenta,
+        colors.cyan,
+        colors.white,
+        colors.background,
+        colors.black_bright,
+        colors.red_bright,
+        colors.green_bright,
+        colors.yellow_bright,
+        colors.blue_bright,
+        colors.magenta_bright,
+        colors.cyan_bright,
+        colors.white_bright,
+        colors.white,
+        colors.white,
+        colors.black,
+        colors.foreground,
+        colors.white,
+        colors.black,
+    );
 }
 
 fn main() {
@@ -180,6 +205,7 @@ fn main() {
             print_color("magenta", colors.magenta, colors.magenta_bright);
             print_color("cyan", colors.cyan, colors.cyan_bright);
             print_color("white", colors.white, colors.white_bright);
+            println!();
         }
     }
 }
